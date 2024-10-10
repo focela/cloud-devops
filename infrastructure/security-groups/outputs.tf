67ec4c0f323756abf6d85347a511c39122bfbd75
Son Tran Thanh <286.trants@gmail.com>
2024-10-10 16:08:11 +0900

    xxxxxx

--- infrastructure/security-groups/outputs.tf
+++ infrastructure/security-groups/outputs.tf
@@ -1,29 +1,29 @@
-# Output the Security Group ID for GitLab
-output "gitlab_security_group_id" {
-  description = "The ID of the security group for GitLab"
+# Output the ID of the GitLab EKS security group
+output "gitlab_eks_security_group_id" {
+  description = "The ID of the GitLab EKS security group"
   value       = aws_security_group.gitlab_eks_sg.id
 }
 
-# Output the Security Group ID for Redis
+# Output the ID of the Redis security group
 output "redis_security_group_id" {
-  description = "The ID of the security group for Redis"
+  description = "The ID of the Redis security group"
   value       = aws_security_group.redis_sg.id
 }
 
-# Output the Security Group ID for PostgreSQL (RDS)
+# Output the ID of the PostgreSQL RDS security group
 output "postgres_security_group_id" {
-  description = "The ID of the security group for PostgreSQL (RDS)"
+  description = "The ID of the PostgreSQL RDS security group"
   value       = aws_security_group.postgres_sg.id
 }
 
-# Output the Security Group ID for EKS nodes
+# Output the ID of the EKS node security group
 output "eks_node_security_group_id" {
-  description = "The ID of the security group for EKS nodes"
+  description = "The ID of the EKS node security group"
   value       = aws_security_group.eks_node_sg.id
 }
 
-# Output the Security Group ID for EKS control plane
+# Output the ID of the EKS control plane security group
 output "eks_control_security_group_id" {
-  description = "The ID of the security group for the EKS control plane"
+  description = "The ID of the EKS control plane security group"
   value       = aws_security_group.eks_control_sg.id
 }
--- infrastructure/security-groups/variables.tf
+++ infrastructure/security-groups/variables.tf
@@ -1,41 +1,18 @@
-# VPC ID where the security groups will be created
+# VPC ID where the security groups will be applied
 variable "vpc_id" {
-  description = "The VPC ID where the security groups will be deployed"
+  description = "The ID of the VPC where security groups will be applied"
   type        = string
 }
 
-# CIDR blocks allowed to access the services (GitLab, Redis, PostgreSQL, etc.)
+# List of allowed CIDR blocks for inbound traffic
 variable "allowed_cidr_blocks" {
-  description = "List of CIDR blocks allowed to connect to the services"
+  description = "List of CIDR blocks allowed for inbound traffic"
   type        = list(string)
 }
 
-# List of Security Group IDs for Redis
-variable "redis_sg_id" {
-  description = "The security group ID for Redis"
-  type        = string
-}
-
-# List of Security Group IDs for PostgreSQL (RDS)
-variable "postgres_sg_id" {
-  description = "The security group ID for PostgreSQL (RDS)"
-  type        = string
-}
-
-# List of Security Group IDs for GitLab
-variable "gitlab_sg_id" {
-  description = "The security group ID for GitLab"
-  type        = string
-}
-
-# List of Security Group IDs for EKS nodes
-variable "eks_node_sg_id" {
-  description = "The security group ID for EKS nodes"
-  type        = string
-}
-
-# List of Security Group IDs for EKS control plane
-variable "eks_control_sg_id" {
-  description = "The security group ID for EKS control plane"
-  type        = string
+# VPC Security Group IDs for additional integrations (optional)
+variable "vpc_security_group_ids" {
+  description = "List of VPC Security Group IDs for additional integrations"
+  type        = list(string)
+  default     = []
 }
