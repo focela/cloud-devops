ab4e9bab50e420320e9d0db1fd19dc0f54df61d3
Son Tran Thanh <286.trants@gmail.com>
2024-10-17 00:50:43 +0700

    Initial commit

--- environments/staging/main.tf
+++
@@ -1,9 +0,0 @@
-# Triển khai module VPC
-module "vpc" {
-  source = "../../modules/vpc"  # Đường dẫn đến module VPC
-  vpc_name = "focela-staging-vpc"
-  vpc_cidr = var.vpc_cidr
-  public_subnet_cidrs = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
-  azs = var.azs
-}
--- environments/staging/provider.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" {
-  region = var.aws_region          # Sử dụng biến cho khu vực AWS
-}
